0dd35366c5eac580ec58562d911e45b0f9867b65
wolfgarr15 <wolfgarr15@gmail.com>
2015-11-12 10:59:31 -0600

    Publishes ENDGINE_TWO branch.

---
+++ ENDGINE_TWO/Core/ECore.cpp
@@ -0,0 +1,27 @@
+// Core.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+#include "ECore.h"
+
+// This is the constructor of a class that has been exported.
+// see Core.h for the class definition
+ECore::ECore()
+{
+	return;
+}
+
+ECore::~ECore()
+{
+	return;
+}
+
+bool ECore::Run()
+{
+	return true;
+}
+
+void ECore::Shutdown()
+{
+	return;
+}
